0e16e1df362ebf51f772f00a781793ed90907b39
akshay <akshayshnanda@gmail.com>
2024-07-11 00:34:48 +0530

    update

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,2 @@
-echo " BUILD START "
-python3.12 -m pip install --upgrade pip
-python3.12 -m pip install -r requirements.txt
-python3.12 manage.py collectstatic --noinput --clear
-echo " BUILD END "
+pip install -r requirements.txt
+python manage.py collectstatic --noinput
