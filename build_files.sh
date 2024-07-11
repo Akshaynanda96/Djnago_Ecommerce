1b22a5af7db70fda69286b6a6b3a504bc6127852
akshay <akshayshnanda@gmail.com>
2024-07-11 15:17:54 +0530

    update

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,7 @@
 echo "BUILD START"
+pip install virtualenv
 source  my_env/Scripts/activate
+cd Ecommerce
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 echo "BUILD END"
