4a0fbd749df1722d5916e7e74fd177e1116c2baa
akshay <akshayshnanda@gmail.com>
2024-07-11 14:40:34 +0530

    update

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,18 @@
+echo "BUILD START"
+
+# create a virtual environment named 'venv' if it doesn't already exist
+python3.9 -m venv my_env
+
+# activate the virtual environment
+source my_env/Scripts/activate
+
+# install all deps in the venv
 pip install -r requirements.txt
-python3.12 manage.py collectstatic
+
+# collect static files using the Python interpreter from venv
+python manage.py collectstatic --noinput
+
+echo "BUILD END"
+
+# [optional] Start the application here 
+# python manage.py runserver
