9f33a5b3555e766fd8771f528bb99d7742ce39e5
akshay <akshayshnanda@gmail.com>
2024-07-11 22:12:40 +0530

    update

--- build_files.sh
+++ build_files.sh
@@ -1,9 +1,9 @@
 echo "BUILD START"
-python3.12 -m pip install Django
 python3.12 -m pip install virtualenv
 python3.12 -m venv my_env
-source  my_env/Scripts/activate
+my_env/Scripts/activate
 cd Ecommerce
+python3.12 -m pip install Django
 python3.12 -m pip install -r requirements.txt
 python3.12 manage.py collectstatic --noinput
 echo "BUILD END"
