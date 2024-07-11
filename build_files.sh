2b3fbe8aa2270d6bbec15484421bb3945f8e21b3
akshay <akshayshnanda@gmail.com>
2024-07-11 15:03:47 +0530

    update

--- build_files.sh
+++ build_files.sh
@@ -1,22 +1,6 @@
-#!/bin/bash
-
 echo "BUILD START"
-
-# Activate the virtual environment
-source my_env/Scripts/activate
-
-# Ensure pip and Python are accessible
+source  my_env/Scripts/activate
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
-
-# Verify the staticfiles_build directory
-if [ ! -d "staticfiles_build" ]; then
-    echo "Error: No Output Directory named 'staticfiles_build' found after the Build completed."
-    echo "You can configure the Output Directory in your Project Settings."
-    exit 1
-fi
-
 echo "BUILD END"
 
-# Optionally, start the application
-# python manage.py runserver
